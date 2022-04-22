2954582e3c6e924ea4d24b6fecb5d8c7756453b4
kkc <kkc@kkcui-MacBookPro.local>
2022-04-22 16:10:42 +0900

    update : 투두리스트 목록 조회로 기능 변경

--- find.js
+++ find.js
@@ -1,33 +1,31 @@
-import { UpdateTodoList } from "./main.js";
-const FindTodo = document.getElementById("find-input");
-const FindBtn = document.querySelector(".modal--find__btn--submit");
-const HeaderFindBtn = document.querySelector(".content-header--find");
-const todoList = document.getElementById("new_todoList");
-const ContentFind = document.querySelector(".content-body--modal--find");
-const ContentNew = document.querySelector(".content-body--modal--new");
-FindBtn.addEventListener("click", FindSubmit);
-HeaderFindBtn.addEventListener("click", (e) => {
-  ContentFind.classList.add("show");
-  ContentNew.classList.remove("show");
-  todoList.innerHTML = "";
-  for (let i = 0; i < localStorage.length; i++) {
-    UpdateTodoList(i);
-  }
-});
-function FindSubmit(e) {
-  todoList.innerHTML = "";
-  if (!FindTodo.value) {
-    for (let i = 0; i < localStorage.length; i++) {
-      UpdateTodoList(i);
-    }
-  } else {
-    for (let i = 0; i < localStorage.length; i++) {
-      if (
-        JSON.parse(localStorage.getItem(i)).Todo.search(FindTodo.value) !== -1
-        /* search : 찾는 문장이 포함되어 있지 않으면 -1 리턴*/
-      ) {
-        UpdateTodoList(i);
+import * as doc from "./document.js";
+const geocoder = new kakao.maps.services.Geocoder();
+
+function getReverseGeocoding(longitude, latitude) {
+  return new Promise((res, rej) =>
+    geocoder.coord2RegionCode(longitude, latitude, (msg, status) => {
+      if (status === kakao.maps.services.Status.OK) {
+        res(msg[0].address_name);
+      } else {
+        return rej(new Error("Bad Status"));
       }
-    }
-  }
+    })
+  );
+}
+
+doc.headerFindBtn.addEventListener("click", searchTodo);
+
+function searchTodo(e) {
+  doc.$contentNew.classList.remove("show");
+  doc.$todoList.innerHTML = "";
+  const todoList = JSON.parse(localStorage.getItem("todo"));
+  todoList.forEach((item) => {
+    getReverseGeocoding(item.longitude, item.latitude).then((reverseGeo) => {
+      const todo = document.createElement("li");
+      const title = document.createTextNode(item.title + " - " + reverseGeo);
+      todo.setAttribute("key", item.id);
+      todo.appendChild(title);
+      doc.$todoList.appendChild(todo);
+    });
+  });
 }
