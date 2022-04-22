6b1c96c79e24c789f32d3fc185a957fb248a385f
kkc <kkc@kkcui-MacBookPro.local>
2022-04-22 18:54:58 +0900

    feat : 삭제에 필요한 요소 추가중

--- find.js
+++ find.js
@@ -1,4 +1,5 @@
 import * as doc from "./document.js";
+import { deleteTodo } from "./delete.js";
 const geocoder = new kakao.maps.services.Geocoder();
 
 function getReverseGeocoding(longitude, latitude) {
@@ -16,16 +17,48 @@ function getReverseGeocoding(longitude, latitude) {
 doc.headerFindBtn.addEventListener("click", searchTodo);
 
 function searchTodo(e) {
-  doc.$contentNew.classList.remove("show");
+  doc.$contentNew.classList.add("hidden");
+  doc.$todoList.classList.remove("hidden");
   doc.$todoList.innerHTML = "";
   const todoList = JSON.parse(localStorage.getItem("todo"));
   todoList.forEach((item) => {
-    getReverseGeocoding(item.longitude, item.latitude).then((reverseGeo) => {
-      const todo = document.createElement("li");
-      const title = document.createTextNode(item.title + " - " + reverseGeo);
-      todo.setAttribute("key", item.id);
-      todo.appendChild(title);
-      doc.$todoList.appendChild(todo);
-    });
+    getReverseGeocoding(item.longitude, item.latitude)
+      .then((reverseGeo) => {
+        const icon = document.createElement("i");
+        icon.setAttribute("class", "fas fa-solid fa-trash");
+        icon.onclick = deleteTodo;
+        icon.id = `${item.id}deleteIcon`;
+        const todo = document.createElement("li");
+        const title = document.createTextNode(
+          item.title + " - " + reverseGeo + " "
+        );
+        const chkBox = document.createElement("input");
+        chkBox.type = "checkbox";
+        chkBox.value = item.done;
+        chkBox.onclick = onClickChkBox;
+        chkBox.id = `${item.id}chkBox`;
+        item.done ? (chkBox.checked = true) : (chkBox.checked = false);
+        todo.setAttribute("key", item.id);
+        todo.appendChild(title);
+        todo.appendChild(chkBox);
+        todo.appendChild(icon);
+        doc.$todoList.appendChild(todo);
+      })
+      .catch((err) => {
+        throw new Error(err.message);
+      });
   });
+}
+
+function onClickChkBox(e) {
+  const todoList = JSON.parse(localStorage.getItem("todo"));
+  let newTodoList = [];
+  todoList.map((item) => {
+    if (item.id == parseInt(e.target.id)) {
+      item.done = !item.done;
+    }
+    newTodoList.push(item);
+  });
+  localStorage.clear();
+  localStorage.setItem("todo", JSON.stringify(newTodoList));
 }
