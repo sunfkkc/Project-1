7cd59191564ec683eca2bb0259c67a6ea28a1db5
kkc <kkc@kkcui-MacBookPro.local>
2022-04-22 16:09:48 +0900

    update : 스토리지에 리스트 형식으로 데이터 저장

--- main.js
+++ main.js
@@ -1,75 +1,42 @@
-/*  */
-const NewBtn = document.querySelector(".content-header--new");
-const ContentFind = document.querySelector(".content-body--modal--find");
-const ContentNew = document.querySelector(".content-body--modal--new");
-NewBtn.addEventListener("click", () => {
-  ContentFind.classList.remove("show");
-  ContentNew.classList.add("show");
-});
+import * as doc from "./document.js";
 
-/* get current Position */
-let ListCount = localStorage.length;
+if (localStorage.length === 0) {
+  localStorage.setItem("todo", JSON.stringify([]));
+}
+let listCount = JSON.parse(localStorage.getItem("todo")).length;
 
-const Data = {
-  latitude: 0,
-  longitude: 0,
-  LocalName: "",
-  Todo: "",
-  done: false,
-};
-
-const geocoder = new kakao.maps.services.Geocoder();
-const GetPositionOk = function (pos) {
-  Data.latitude = pos.coords.latitude;
-  Data.longitude = pos.coords.longitude;
-  Data.Todo = document.getElementById("new-input").value;
-  const callback = function (result, status) {
-    if (status === kakao.maps.services.Status.OK) {
-      Data.LocalName = result[0].address_name;
-      localStorage.setItem(ListCount, JSON.stringify(Data));
-      UpdateTodoList(ListCount);
-      ListCount++;
-    }
-  };
-  geocoder.coord2RegionCode(
-    pos.coords.longitude,
-    pos.coords.latitude,
-    callback
+function getPosition() {
+  return new Promise((res, rej) =>
+    navigator.geolocation.getCurrentPosition(res, rej)
   );
-};
-const NoPosition = function (err) {
-  console.log(err);
-  throw new Error(err.message);
-};
-
-/* 작성 버튼 눌렀을 때 */
-const newSubmit = function (e) {
-  const newContent = document.getElementById("new-input").value;
-  if (!newContent) {
+}
+/* 제출 후 로컬 스토리지 저장 */
+const addTodo = async function () {
+  const newContent = doc.newTodo.value;
+  if (!newContent || !doc.newTodo) {
     throw new Error("invalid content");
   }
-  const todoList = document.getElementById("new_todoList");
-  navigator.geolocation.getCurrentPosition(
-    GetPositionOk,
-    NoPosition
-  ); /* 현재 위치를 불러오는데 시간이 걸려서 바로 리스트에 등록이 안됨 프로미스를 써서 해결해 볼수 있을까..*/
+  doc.newBtn.classList.add("hidden");
+  const loadingLabel = document.createElement("label");
+  const loadingText = document.createTextNode("Loading...");
+  loadingLabel.appendChild(loadingText);
+  doc.$contentNew.appendChild(loadingLabel);
+  getPosition()
+    .then((pos) => {
+      const todo = {
+        id: listCount++,
+        title: doc.newTodo.value,
+        longitude: pos.coords.longitude,
+        latitude: pos.coords.latitude,
+      };
+      const todoList = JSON.parse(localStorage.getItem("todo"));
+      localStorage.setItem("todo", JSON.stringify([...todoList, todo]));
+      doc.$contentNew.removeChild(loadingLabel);
+      doc.newBtn.classList.remove("hidden");
+    })
+    .catch((err) => {
+      doc.$contentNew.removeChild(loadingLabel);
+      throw new Error(err.message);
+    });
 };
-const newbtnSubmit = document.querySelector(".modal--new__btn--submit");
-newbtnSubmit.addEventListener("click", newSubmit);
-
-/* bring todoList from localstorage */
-export function UpdateTodoList(index) {
-  const todoList = document.getElementById("new_todoList");
-  todoList.innerHTML += `<li key=${index} class=TodoList${index}>${
-    JSON.parse(localStorage.getItem(index)).Todo
-  } - ${JSON.parse(localStorage.getItem(index)).LocalName} 
-  <span> <input type="checkbox" class=TodoListChk${index}></span>
-  </li>`;
-}
-const todoList = document.getElementById("new_todoList");
-for (let i = 0; i < localStorage.length; i++) {
-  UpdateTodoList(i);
-}
-
-/*  체크박스 눌렀을 때 done 처리*/
-const DoneTodoList = document.querySelector(".TodoList");
+doc.newBtn.addEventListener("click", addTodo);
