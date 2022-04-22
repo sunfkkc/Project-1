d485d6dab6d577a62145844f8ab728139a49e344
kkc <kkc@kkcui-MacBookPro.local>
2022-04-22 18:56:00 +0900

    fix: loading 중일때 입력 불가

--- main.js
+++ main.js
@@ -5,18 +5,34 @@ if (localStorage.length === 0) {
 }
 let listCount = JSON.parse(localStorage.getItem("todo")).length;
 
+/* get 위도 경도 */
 function getPosition() {
   return new Promise((res, rej) =>
     navigator.geolocation.getCurrentPosition(res, rej)
   );
 }
-/* 제출 후 로컬 스토리지 저장 */
-const addTodo = async function () {
+
+/* 위도 경도를 이용한 reverse geocoding */
+function getReverseGeocoding(longitude, latitude) {
+  return new Promise((res, rej) =>
+    geocoder.coord2RegionCode(longitude, latitude, (msg, status) => {
+      if (status === kakao.maps.services.Status.OK) {
+        res(msg[0].address_name);
+      } else {
+        return rej(new Error("Bad Status"));
+      }
+    })
+  );
+}
+
+const addTodo = function () {
+  /* 제출 후 로컬 스토리지 저장 */
   const newContent = doc.newTodo.value;
   if (!newContent || !doc.newTodo) {
     throw new Error("invalid content");
   }
   doc.newBtn.classList.add("hidden");
+  doc.newTodo.readOnly = true;
   const loadingLabel = document.createElement("label");
   const loadingText = document.createTextNode("Loading...");
   loadingLabel.appendChild(loadingText);
@@ -28,11 +44,13 @@ const addTodo = async function () {
         title: doc.newTodo.value,
         longitude: pos.coords.longitude,
         latitude: pos.coords.latitude,
+        done: false,
       };
       const todoList = JSON.parse(localStorage.getItem("todo"));
       localStorage.setItem("todo", JSON.stringify([...todoList, todo]));
       doc.$contentNew.removeChild(loadingLabel);
       doc.newBtn.classList.remove("hidden");
+      doc.newTodo.readOnly = false;
     })
     .catch((err) => {
       doc.$contentNew.removeChild(loadingLabel);
