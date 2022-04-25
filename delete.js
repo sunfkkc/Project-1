29ceb8db1823f74bfeb8dda829464c22d76c83e4
KIMKICHAN <rlcks980705@gmail.com>
2022-04-25 19:47:34 +0900

    feat : 삭제 아이콘을 누르면 로컬스토리지에 해당값 삭제 후 id 정렬

--- delete.js
+++ delete.js
@@ -1,6 +1,13 @@
 import { asyncInsertTodoList } from "./insertTodoList.js";
 
-export const deleteTodo = function (e) {
+export const makeDeleteIcon = function (item) {
+  const deleteIcon = document.createElement("i");
+  deleteIcon.setAttribute("class", "fas fa-solid fa-trash");
+  deleteIcon.onclick = deleteTodo;
+  deleteIcon.id = `${item.id}deleteIcon`;
+  return deleteIcon;
+};
+const deleteTodo = function (e) {
   const beforeDeletTodoList = JSON.parse(localStorage.getItem("todo"));
   const deletedTodoList = beforeDeletTodoList.filter(
     (item) => item.id !== parseInt(e.target.id)
