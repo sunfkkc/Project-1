80594b394d7b12f0bdfbea1802a7c80ef6ab33fb
KIMKICHAN <rlcks980705@gmail.com>
2022-04-25 19:46:14 +0900

    feat : 체크하면 로컬스토리지에 done처리

---
+++ check.js
@@ -0,0 +1,22 @@
+export const makeCheckBox = function (item) {
+  const chkBox = document.createElement("input");
+  chkBox.type = "checkbox";
+  chkBox.value = item.done;
+  chkBox.onclick = onClickChkBox;
+  chkBox.id = `${item.id}chkBox`;
+  chkBox.setAttribute("class", "chkBox");
+  item.done ? (chkBox.checked = true) : (chkBox.checked = false);
+  return chkBox;
+};
+function onClickChkBox(e) {
+  const todoList = JSON.parse(localStorage.getItem("todo"));
+  let newTodoList = [];
+  todoList.map((item) => {
+    if (item.id == parseInt(e.target.id)) {
+      item.done = !item.done;
+    }
+    newTodoList.push(item);
+  });
+  localStorage.clear();
+  localStorage.setItem("todo", JSON.stringify(newTodoList));
+}
