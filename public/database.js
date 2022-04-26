4fd1c86106b5a0a39220d2c31e5806ea1c28d92f
KIMKICHAN <rlcks980705@gmail.com>
2022-04-26 16:33:17 +0900

    feat : db 분리

---
+++ public/database.js
@@ -0,0 +1,10 @@
+const TODOS_KEY = "todos";
+
+export class Database {
+  static load() {
+    return JSON.parse(localStorage.getItem(TODOS_KEY)) ?? [];
+  }
+  static save(todos) {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+  }
+}
