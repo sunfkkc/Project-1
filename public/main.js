646d85292639e9da18a68f3782935d0b62887d21
KIMKICHAN <rlcks980705@gmail.com>
2022-04-26 18:22:03 +0900

    refactor : main 함수는 todoList만 생성

--- public/main.js
+++ public/main.js
@@ -1,11 +1,7 @@
 import { Database } from "./database.js";
-import * as doc from "./document.js";
 import { makeTodoList } from "./insertTodoList.js";
-import { getReverseGeocoding } from "./util.js";
 
-let todos = [];
 function main() {
-  todos = Database.load();
   makeTodoList(todos);
 }
 
