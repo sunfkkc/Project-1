58c820f2ebd7c2b74619c73fdf12b685ac079c4e
KIMKICHAN <rlcks980705@gmail.com>
2022-04-26 17:57:44 +0900

    refactor : todos를 전역 변수로 선언

--- public/main.js
+++ public/main.js
@@ -1,53 +1,40 @@
 import { Database } from "./database.js";
 import * as doc from "./document.js";
-import { asyncInsertTodoList } from "./insertTodoList.js";
+import { InsertTodoList } from "./insertTodoList.js";
+import { getReverseGeocoding } from "./util.js";
 
-/* 새로고침 할때 투두리스트 불러오기 */
-if (localStorage.length === 0) {
-  //localStorage.setItem("todo", JSON.stringify([]));
-  Database.save([]);
-} else {
-  asyncInsertTodoList(Database.load());
+let todos = [];
+function main() {
+  todos = Database.load();
+  InsertTodoList(todos);
 }
 
-/* get 위도 경도 */
-function getPosition() {
-  return new Promise((res, rej) =>
-    navigator.geolocation.getCurrentPosition(res, rej)
-  );
-}
-
-const addTodo = function () {
-  /* 제출 후 로컬 스토리지 저장 */
-  const newContent = doc.newTodo.value;
-  if (!newContent || !doc.newTodo) {
-    //throw new Error("invalid content");
-    return alert("할 일을 입력해 주세요!");
-  }
+function isLoading() {
   doc.newBtn.classList.add("hidden");
   doc.newTodo.readOnly = true;
   doc.$contentNew.appendChild(doc.loadingLabel);
-  getPosition()
-    .then((pos) => {
-      const todo = {
-        id: Database.load().length,
-        title: doc.newTodo.value,
-        longitude: pos.coords.longitude,
-        latitude: pos.coords.latitude,
-        done: false,
-      };
-      const todoList = Database.load();
-      todoList.push(todo);
-      //localStorage.setItem("todo", JSON.stringify(todoList));
-      Database.save(todoList);
-      doc.$contentNew.removeChild(doc.loadingLabel);
-      doc.newBtn.classList.remove("hidden");
-      doc.newTodo.readOnly = false;
-      doc.newTodo.value = "";
-      asyncInsertTodoList(todoList);
-    })
-    .catch((err) => {
-      throw new Error(err.message);
-    });
+}
+function endLoading() {
+  doc.$contentNew.removeChild(doc.loadingLabel);
+  doc.newBtn.classList.remove("hidden");
+  doc.newTodo.readOnly = false;
+  doc.newTodo.value = "";
+}
+const addTodo = async function () {
+  const newContent = doc.newTodo.value;
+  if (!newContent || !doc.newTodo) {
+    return alert("할 일을 입력해 주세요!");
+  }
+  isLoading();
+  const newTodo = {
+    id: todos.length,
+    title: doc.newTodo.value,
+    location: await getReverseGeocoding(),
+    done: false,
+  };
+  todos.push(newTodo);
+  Database.save(todos);
+  endLoading();
 };
 doc.newBtn.addEventListener("click", addTodo);
+main();
